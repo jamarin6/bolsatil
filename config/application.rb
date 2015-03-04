3a7c5369df3b52d0f99c2a8e3c64d60887ed0c4c
camumino <camumino@gmail.com>
2015-03-04 10:21:45 +0100

    arreglo assets

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Bolsatil
     # config.i18n.default_locale = :de
 
     # Enable the asset pipeline
-    #config.assets.enabled = true
+    config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
